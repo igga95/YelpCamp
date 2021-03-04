b092dde5b21c8632a17a8c0fbdb64ba462b4b1c8
Gabriel <iglesiasg95@gmail.com>
2021-03-04 11:11:17 -0300

    Creation and styling of Cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -5,10 +5,6 @@ const campground = require('../models/campground');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelper');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-console.log(mapBoxToken);
-const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -26,14 +22,9 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 500; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
-        const geoData = await geocoder.forwardGeocode({
-            query: `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            limit: 1
-        }).send();
-        // console.log(geoData);
         const camp = new Campground({
             author: '601a937392b2612426349ac7',
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
@@ -46,7 +37,13 @@ const seedDB = async () => {
                 url: 'https://res.cloudinary.com/dr0nzbfe1/image/upload/v1614039506/YelpCamp/tent_su6sqr.jpg',
                 filename: 'YelpCamp/tent_su6sqr'
             }],
-            geometry: geoData.body.features[0].geometry,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[rand1000].longitude,
+                    cities[rand1000].latitude,
+                ]
+            },
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia deserunt excepturi est nostrum. Iste animi, eaque nulla mollitia, nemo voluptas quaerat nostrum aperiam quis minus, accusantium id tempora doloremque reiciendis.',
             price
         });
