c411fd38239bbf0970e757c0ea60f957796394cc
Gabriel Iglesias <iglesiasg95@gmail.com>
2022-01-14 11:20:20 -0300

    fix process env port variable

--- app.js
+++ app.js
@@ -135,6 +135,7 @@ app.use((err, req, res, next) => {
     next();
 });
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
 });
